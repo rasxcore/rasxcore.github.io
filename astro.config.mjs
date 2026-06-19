102cd9c80e3989f614a8d2be9e6267c4c18833ee
Ras <151653920+rasxcore@users.noreply.github.com>
2026-06-19 13:33:15 +0300

    remove outdated GitHub Pages comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// GitHub Pages user site (username.github.io): base = '/'
 export default defineConfig({
   site: 'http://rasxcore.ru/',
   output: 'static',
